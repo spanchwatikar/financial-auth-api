24f46a005a8002121466801b7e2b0f3841952054
spanchwatikar <109857128+spanchwatikar@users.noreply.github.com>
2022-10-17 22:52:44 +0530

    application setup

---
+++ src/main/java/com/cloud/financial/service/auth/FinancialAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.financial.service.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialAuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/financial/service/auth/FinancialAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.financial.service.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
